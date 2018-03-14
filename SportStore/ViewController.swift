3c10ba88da13bae006ebe279516c8421341fc2ea
am903103 <am903103@au.edu.tw>
2018-03-14 16:04:04 +0800

    Add fake data, binding dataSource/Delegate

--- SportStore/ViewController.swift
+++ SportStore/ViewController.swift
@@ -8,7 +8,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return products.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let product = products[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ProductCell", for: indexPath)
+        cell.textLabel?.text = product.0
+        return cell
+    }
+
+    var products = [
+        ("Kayak", "A boat for one person", "Watersports", 275.0, 10),
+        ("Lifejacket", "Protective and fashionable", "Watersports", 48.95, 14),
+        ("Soccer Ball", "FIFA-approved size and weight", "Soccer", 19.5, 32),
+        ("Corner Flags", "Give your playing field a professional touch",
+                "Soccer", 34.95, 1),
+        ("Stadium", "Flat-packed 35,000-seat stadium", "Soccer", 79500.0, 4),
+        ("Thinking Cap", "Improve your brain efficiency by 75%", "Chess", 16.0, 8),
+        ("Unsteady Chair", "Secretly give your opponent a disadvantage",
+                "Chess", 29.95, 3),
+        ("Human Chess Board", "A fun game for the family", "Chess", 75.0, 2),
+        ("Bling-Bling King", "Gold-plated, diamond-studded King",
+                "Chess", 1200.0, 4)];
 
     override func viewDidLoad() {
         super.viewDidLoad()
